d9e0330177da402f2dabb394f715b883388ead57
zhangtian <zhangtian@DESKTOP-JF9M0VD>
2016-07-14 15:34:50 +0800

    xml解析封装测试

---
+++ xmlparser-demo/src/main/java/xml_parser/xmlparser_demo/App.java
@@ -0,0 +1,13 @@
+package xml_parser.xmlparser_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
