d98aff26fda7ca195ec6d3c9429e928fb253a194
zhangtian <zhangtian@DESKTOP-JF9M0VD>
2016-07-14 16:15:38 +0800

    XML解析测试

---
+++ xmlparser-demo/src/test/java/cn/itcast/xml/parser/test/XMLParserTest.java
@@ -0,0 +1,62 @@
+package cn.itcast.xml.parser.test;
+
+import java.util.List;
+
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.XMLConfiguration;
+import org.apache.commons.configuration.tree.xpath.XPathExpressionEngine;
+import org.junit.Test;
+import org.w3c.dom.NodeList;
+
+public class XMLParserTest {
+
+	@Test
+	public void testSimpleXmlParser() throws ConfigurationException {
+		XMLConfiguration configuration = new XMLConfiguration(this.getClass().getClassLoader().getResource("simple.xml")) ;
+		System.out.println(configuration.getString("database.url"));
+		System.out.println(configuration.getInt("database.port"));
+		System.out.println(configuration.getString("database.login"));
+	}
+	
+	@Test
+	public void testMultiXmlParser() throws ConfigurationException {
+		XMLConfiguration configuration = new XMLConfiguration(this.getClass().getClassLoader().getResource("multi.xml")) ;
+		// configuration.setDelimiterParsingDisabled(true);
+		System.out.println(configuration.getString("databases.database(0).url"));
+		System.out.println(configuration.getList("databases.database(1).password"));
+		
+		System.out.println("===========================");
+		
+		List<Object> list = configuration.getList("person.address") ;
+		System.out.println(list);
+		System.out.println(configuration.getList("person.info[@count]"));
+	}
+	
+	@Test
+	public void testXPath() throws ConfigurationException {
+		XMLConfiguration config = new XMLConfiguration(this.getClass().getClassLoader().getResource("multi.xml"));
+		config.setExpressionEngine(new XPathExpressionEngine());
+		
+		// 127.0.0.1
+		String dev =  config.getString("databases/database[name = 'dev']/url");       
+		 
+		// 192.23.44.100
+		String production = config.getString("databases/database[name = 'production']/url");
+		System.out.println(dev);
+		System.out.println(production);
+	}
+	
+	@Test
+	public void testOther() throws ConfigurationException {
+		XMLConfiguration config = new XMLConfiguration(this.getClass().getClassLoader().getResource("multi.xml"));
+		
+		NodeList list = config.getDocument().getElementsByTagName("database") ;
+		System.out.println(list.getLength());
+		for(int i = 0; i < list.getLength(); i++) {
+			NodeList nodes = list.item(i).getChildNodes() ;
+			for (int j = 0; j < nodes.getLength(); j++) {
+				System.out.println(nodes.item(j).getTextContent());
+			}
+		}
+	}
+}
